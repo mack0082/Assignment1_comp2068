fc38b66fd38f14a327f9d0ebe261d97f352fbf6a
mack0082 <mack.waters0803@gmail.com>
2017-02-10 15:10:17 -0500

    all pages updated

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
-/* GET  page. */
+/* GET about page. */
 router.get('/about', function (req, res, next) {
     res.render('about', { title: 'About Me' });
 });
-/* GET  page. */
+/* GET projects page. */
 router.get('/projects', function (req, res, next) {
     res.render('projects', { title: 'Projects' });
 });
-/* GET  page. */
+/* GET  services page. */
 router.get('/services', function (req, res, next) {
     res.render('services', { title: 'Services' });
 });
-/* GET  page. */
+/* GET contact page. */
 router.get('/contact', function (req, res, next) {
     res.render('contact', { title: 'Contact' });
 });
+
+
 module.exports = router;
